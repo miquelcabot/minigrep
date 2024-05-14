0ef5ea0938b3267fde1ed31ae774789b9c11a230
miquelcabot <miquel.cabot@gmail.com>
2024-05-14 11:42:09 +0200

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -38,3 +38,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
     results
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
